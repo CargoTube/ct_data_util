457a26a8ea0fec4acb8efcc2d108b611ae1241c7
bwegh <baswegh@gmx.de>
2018-08-16 14:42:09 +0200

    add table set/get version functions

--- src/ct_data_util.erl
+++ src/ct_data_util.erl
@@ -9,7 +9,10 @@
          create_mnesia_schema_if_needed/0,
 
          setup_sqlite_if_needed/0,
-         get_sqlite_connection/0
+         get_sqlite_connection/0,
+
+         get_table_version/1,
+         set_table_version/2
         ]).
 
 stop_mnesia() ->
@@ -48,13 +51,53 @@ setup_sqlite_if_needed() ->
 setup_sqlite_if_needed(undefined) ->
     SqliteFile = application:get_env(ct_data_util, mnesia_dir, ?SQLITE),
     {ok, Connection} = esqlite3:open(SqliteFile),
-    Sql = "CREATE TABLE IF NOT EXISTS ctdatainfo ( name TEXT NOT NULL UNIQUE"
-        ", version TEXT NOT NULL)",
-    true = is_integer(esqlite3:exec(Sql, Connection)),
+    Sql = "CREATE TABLE IF NOT EXISTS ctdatainfo ( name TEXT PRIMARY KEY"
+        ", version REAL NOT NULL)",
+    ok = esqlite3:exec(Sql, Connection),
     application:set_env(ct_data_util, sqlite_connection, Connection),
     ok;
 setup_sqlite_if_needed({ok, _}) ->
     ok.
+
+
+get_table_version(Table) ->
+    MaybeCon = get_sqlite_connection(),
+    maybe_get_table_version(Table, MaybeCon).
+
+maybe_get_table_version(Table, {ok, Con}) ->
+    Result = esqlite3:q("SELECT version FROM ctdatainfo WHERE name = ?",
+                        [Table], Con),
+    handle_version_result(Result);
+maybe_get_table_version(_Table, _NoCon) ->
+    {error, not_connected}.
+
+handle_version_result([]) ->
+    {error, not_found};
+handle_version_result([Version]) ->
+    {ok, Version};
+handle_version_result(Other) ->
+    Other.
+
+
+set_table_version(Table, Version) ->
+    MaybeCon = get_sqlite_connection(),
+    maybe_set_table_version(Table, Version, MaybeCon).
+
+
+maybe_set_table_version(Table,  Version, {ok, Con}) ->
+    Result = esqlite3:q("INSERT INTO ctdatainfo (table, version) VALUES (?, ?)",
+                        [Table, Version], Con),
+    handle_set_version_result(Result);
+maybe_set_table_version(_Table, _Version, _NoCon) ->
+    {error, not_connected}.
+
+handle_set_version_result([]) ->
+    {error, not_found};
+handle_set_version_result([Version]) ->
+    {ok, Version};
+handle_set_version_result(Other) ->
+    Other.
+
 
 get_sqlite_connection() ->
     application:get_env(ct_data_util, sqlite_connection).
